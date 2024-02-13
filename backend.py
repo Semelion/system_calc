2afa15104aa834eba2d73aa3589fbe31a51d7ba8
irina babynina <115956515+irisich@users.noreply.github.com>
2024-02-13 23:51:35 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,39 +1,50 @@
-def toBASEint(num, base):
+ def toBASEint(num, base):
     alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     n = abs(num)
     b = alpha[n % base]
-    while n >= base :
+    while n >= base:
         n = n // base
         b += alpha[n % base]
     return ('' if num >= 0 else '-') + b[::-1]
 
-def toBaseFrac(frac, base, n = 16) :
+
+def toBaseFrac(frac, base, n=16):
     alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     b = ''
-    while n :
+    while n:
         frac *= base
-        frac = round(frac,n)
+        frac = round(frac, n)
         b += str(alpha[int(frac)])
         frac -= int(frac)
         n -= 1
     return b
 
-Number = input()
-Basein = int(input())
-Baseout = int(input())
-alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-if '.' in Number :
-    num, frac = map(str,Number.split('.'))
-    num = int(num,Basein)
-    a = toBASEint(num,Baseout)
-    b = 0
-    k = Basein
-    for i in frac :
-        b += alpha.index(i) / k
-        k *= Basein
-    b = str(toBaseFrac(b, Baseout)).rstrip('0')
-    print(a+'.'+b)
-else :
-    print(toBASEint(int(Number,Basein),Baseout))
+def convert(Number, Basein, Baseout):
+    alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if '.' in Number:
+        num, frac = map(str, Number.split('.'))
+        num = int(num, Basein)
+        a = toBASEint(num, Baseout)
+        b = 0
+        k = Basein
+        for i in frac:
+            b += alpha.index(i) / k
+            k *= Basein
+        b = str(toBaseFrac(b, Baseout)).rstrip('0')
+        return(a + '.' + b)
+    else:
+        return(toBASEint(int(Number, Basein), Baseout))
 
-print("end")
+def perform_operation(operation, num_bas1, num1, num_bas2, num2, res_bas):
+    if operation == '+':
+        result = int(convert(num1, num_bas1, 10)) + int(convert(num2, num_bas2, 10))
+    elif operation == '-':
+        result = abs(int(convert(num1, num_bas1, 10)) + int(convert(num2, num_bas2, 10)))
+    elif operation == '*':
+        result = int(convert(num1, num_bas1, 10)) * int(convert(num2, num_bas2, 10))
+    elif operation == ':':
+        result = int(convert(num1, num_bas1, 10)) / int(convert(num2, num_bas2, 10))
+
+def num_systems(operation, num_bas1, num1, num_bas2, num2, res_bas):
+    result = perform_operation(operation, int(num_bas1), num1, int(num_bas2), num2, int(res_bas))
+    return result
